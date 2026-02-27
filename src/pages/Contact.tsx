3d4059b949cff18ce2be57e3a8c31e5456898a76
Ahoueffa-R <ahoueffa.rey@gmail.com>
2026-02-27 18:44:07 +0100

    Update Contact.tsx
    Correction ligne 169 - 171 fermeture avec </Link>

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -168,7 +168,7 @@ export default function Contact() {
             </p>
             <Link to="/dating-pro" className="inline-block bg-white text-green px-6 py-2 rounded-full text-sm font-bold hover:bg-green hover:text-white transition-colors">
               Réserver un créneau
-            </a>
+            </Link>
           </div>
         </div>
 
